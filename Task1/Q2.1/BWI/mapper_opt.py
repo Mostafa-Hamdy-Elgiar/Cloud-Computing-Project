09d2ba8adcdc901cc573c18890561fe8a6639f87
Mostafa Hamdy Elgiar <mos.elgiar@gmail.com>
2018-06-16 14:30:16 +0200

    Create mapper_opt.py

---
+++ Task1/Q2.1/BWI/mapper_opt.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+import sys
+
+
+for row in sys.stdin:
+        row = row.strip()
+        row_list = row.split(',')
+        if row_list[3] == 'BWI' :
+                print row_list[1] + ',' + row_list[6]
