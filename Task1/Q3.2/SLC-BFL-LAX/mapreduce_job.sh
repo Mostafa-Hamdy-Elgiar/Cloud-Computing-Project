b0819d91ce975b4a5657e813921b4f2adbcd823a
Mostafa Hamdy Elgiar <mos.elgiar@gmail.com>
2018-06-16 21:06:47 +0200

    Add files via upload

---
+++ Task1/Q3.2/SLC-BFL-LAX/mapreduce_job.sh
@@ -0,0 +1,6 @@
+/root/Hadoop/hadoop-2.7.3/bin/hadoop jar /root/Hadoop/hadoop-2.7.3/share/hadoop/tools/lib/hadoop-streaming-2.7.3.jar \
+ -input /TransData/MyData.csv \
+ -output /Q3.2/SLC-BFL-LAX \
+ -file /home/ubuntu/Task1/Q7/SLC-BFL-LAX/mapper.py /home/ubuntu/Task1/Q7/SLC-BFL-LAX/reducer.py \
+ -mapper "/usr/bin/python mapper.py" \
+ -reducer "/usr/bin/python reducer.py"
