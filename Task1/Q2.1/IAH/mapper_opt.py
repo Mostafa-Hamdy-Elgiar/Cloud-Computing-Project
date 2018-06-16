02109422b3eb501f0d6f7a8f781d6ab6c45821da
Mostafa Hamdy Elgiar <mos.elgiar@gmail.com>
2018-06-16 14:35:38 +0200

    Create mapper_opt.py

---
+++ Task1/Q2.1/IAH/mapper_opt.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+import sys
+
+for row in sys.stdin:
+        row = row.strip()
+        row_list = row.split(',')
+        if row_list[3] == 'IAH' :
+                print row_list[1] + ',' + row_list[6]
