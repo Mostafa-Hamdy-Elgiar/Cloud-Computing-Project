6a0c78c63dd4a90c16563b304ccb8e263ffeade6
Mostafa Hamdy Elgiar <mos.elgiar@gmail.com>
2018-06-16 17:24:52 +0200

    Add files via upload

---
+++ Task1/Q2.3/JFK-LAX/mapreduce_job.sh
@@ -0,0 +1,6 @@
+/root/Hadoop/hadoop-2.7.3/bin/hadoop jar /root/Hadoop/hadoop-2.7.3/share/hadoop/tools/lib/hadoop-streaming-2.7.3.jar \
+ -input /TransData/MyData.csv \
+ -output /Q2_3/JFK-LAX \
+ -file /home/ubuntu/Task1/Q5/JFK-LAX/mapper_opt.py /home/ubuntu/Task1/Q5/JFK-LAX/reducer_opt.py \
+ -mapper "/usr/bin/python mapper_opt.py" \
+ -reducer "/usr/bin/python reducer_opt.py"
