608e326739cbbedadec10efc96a8e078851a83f5
Mostafa Hamdy Elgiar <mos.elgiar@gmail.com>
2018-06-16 14:24:27 +0200

    Delete mapper_opt.py

--- Task1/Q2.1/CMI/mapper_opt.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-import sys
-
-for row in sys.stdin:
-        row = row.strip()
-        row_list = row.split(',')
-        if row_list[3] == 'CMI' :
-                print row_list[1] + ',' + row_list[6]
