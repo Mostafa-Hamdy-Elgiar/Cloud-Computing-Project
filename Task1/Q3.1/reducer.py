8288b3081072941d02a396790bf516a60d7cf7b6
Mostafa Hamdy Elgiar <mos.elgiar@gmail.com>
2018-06-16 19:51:49 +0200

    Add files via upload

---
+++ Task1/Q3.1/reducer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+from operator import itemgetter 
+import sys
+
+count = 1
+output_dic = {}
+for row in sys.stdin:
+	row = row.strip()
+	Origin , count = row.split(',')
+	count = int(count)
+	output_dic[Origin] = output_dic.get(Origin , 0) + 1
+
+for key, value in sorted(output_dic.iteritems(),key=itemgetter(1) , reverse=True):
+	print "%s: %s" % (key , value)
