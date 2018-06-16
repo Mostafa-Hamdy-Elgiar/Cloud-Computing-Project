e9ab329ad3feee9a2033a0a4b6d50411eab9b7e8
Mostafa Hamdy Elgiar <mos.elgiar@gmail.com>
2018-06-16 16:30:13 +0200

    Create mapper_opt.py

---
+++ Task1/Q2.2/LAX/mapper_opt.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+import sys
+
+for row in sys.stdin:
+        row = row.strip()
+        row_list = row.split(',')
+        if row_list[3] == 'LAX' :
+                print row_list[4] + ',' + row_list[6]
