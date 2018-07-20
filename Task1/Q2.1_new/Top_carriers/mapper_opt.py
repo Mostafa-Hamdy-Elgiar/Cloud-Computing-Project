16c15be7e69e8920616a3d0242bdcec59dbbefc7
Mostafa Hamdy Elgiar <mos.elgiar@gmail.com>
2018-07-20 18:18:48 +0200

    Create mapper_opt.py

---
+++ Task1/Q2.1_new/Top_carriers/mapper_opt.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+import sys
+
+
+for row in sys.stdin:
+        row = row.strip()
+        row_list = row.split(',')
+        print row_list[3] + ',' + row_list[1] + ',' + row_list[6]
