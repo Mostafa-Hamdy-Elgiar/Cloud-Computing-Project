789df27b6c6bb8375e4b6a7e0a10b714d8089e25
Mostafa Hamdy Elgiar <mos.elgiar@gmail.com>
2018-07-20 13:30:49 +0200

    Update mapreduce_job.sh

--- Task1/Q1.1/mapreduce_job.sh
+++ Task1/Q1.1/mapreduce_job.sh
@@ -1,6 +1,6 @@
 /root/Hadoop/hadoop-2.7.3/bin/hadoop jar /root/Hadoop/hadoop-2.7.3/share/hadoop/tools/lib/hadoop-streaming-2.7.3.jar \
  -input /TransData/MyData.csv \
  -output /Q1.1 \
- -file /home/ubuntu/mapper.py /home/ubuntu/reducer.py \
+ -file /home/ubuntu/Task1/Q1.1/mapper.py /home/ubuntu/Task1/Q1.1/reducer.py \
  -mapper "/usr/bin/python mapper.py" \
  -reducer "/usr/bin/python reducer.py"
