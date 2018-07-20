c9ecd76a10e837153bd2a6e6725e27f690fd01d6
Mostafa Hamdy Elgiar <mos.elgiar@gmail.com>
2018-07-20 18:13:13 +0200

    Delete mapreduce_job.sh

--- Task1/Q2.1/BWI/mapreduce_job.sh
+++
@@ -1,6 +0,0 @@
-/root/Hadoop/hadoop-2.7.3/bin/hadoop jar /root/Hadoop/hadoop-2.7.3/share/hadoop/tools/lib/hadoop-streaming-2.7.3.jar \
- -input /TransData/MyData.csv \
- -output /Q2_1/BWI \
- -file /home/ubuntu/Task1/Q3/BWI/mapper_opt.py /home/ubuntu/Task1/Q3/BWI/reducer_opt.py \
- -mapper "/usr/bin/python mapper_opt.py" \
- -reducer "/usr/bin/python reducer_opt.py"
