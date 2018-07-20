bba6c734cd1e21d2107547194617ed72eb9336f4
Mostafa Hamdy Elgiar <mos.elgiar@gmail.com>
2018-07-20 09:09:59 +0200

    Update reducer.py

--- Task1/Q1.1/reducer.py
+++ Task1/Q1.1/reducer.py
@@ -7,12 +7,12 @@ count = 1
 output_dic = {}
 for row in sys.stdin:
 	row = row.strip()
-	Origin , count = row.split(',')
+	airport , count = row.split(',')
 	count = int(count)
-	output_dic[Origin] = output_dic.get(Origin , 0) + count
+	output_dic[airport] = output_dic.get(airport , 0) + count
 
 for key, value in sorted(output_dic.iteritems(),key=itemgetter(1) , reverse=True):
 	print "%s: %s" % (key , value)
 	count += 1
-	if count == 10:
+	if count == 11:
 		break
