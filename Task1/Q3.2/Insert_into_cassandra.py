057c3b34f8ee51f922d266a0c36d78c4b55dcc92
Mostafa Hamdy Elgiar <mos.elgiar@gmail.com>
2018-07-21 09:32:22 +0200

    Update Insert_into_cassandra.py

--- Task1/Q3.2/Insert_into_cassandra.py
+++ Task1/Q3.2/Insert_into_cassandra.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python
 
+from pyspark import SparkContext
 from cassandra.cluster import Cluster
 import os
 
 cluster = Cluster(['127.0.0.1'])
 session = cluster.connect()
 session.set_keyspace('cloudproject')
+sc=SparkContext()
 
 airports = ['CMI-ORD-LAX' , 'DFW-ORD-DFW' , 'JAX-DFW-CRP' , 'LAX-ORD-JFK' , 'LAX-SFO-PHX' , 'SLC-BFL-LAX']
 for path in airports :
-        HDFS_Data = os.popen('/root/Hadoop/hadoop-2.7.3/bin/hadoop fs -cat hdfs://namenode:9000/Q3.2/'+path+'/part-00000')
-        HDFS_Data = HDFS_Data.readlines()
-        print HDFS_Data
-
-        for c in HDFS_Data :
-                airports_path = c.split(':')[0]
-                Flights = c.split(':')[1].strip()
-                session.execute("insert into Best_paths (airports_path,flights) values ('"+airports_path +"','"+Flights+"')")
+        #sc=SparkContext()
+        raw_text = sc.textFile('hdfs://namenode:9000/Q3.2/'+path+'/part-00000' , 1)
+        r1=raw_text.map(lambda x: x.split(':'))
+        rdata = r1.collect()
+        session.execute("insert into Best_paths (airports_path,flights) values ('"+rdata[0][0].strip()+"','"+rdata[0][1].strip()+"')")
