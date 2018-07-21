8b0806b97dc24e8f951fb82191a9af31c6e1fec8
Mostafa Hamdy Elgiar <mos.elgiar@gmail.com>
2018-07-21 09:24:51 +0200

    Update mapreduce_job.sh

--- Task1/Q3.2/DFW-ORD-DFW/mapreduce_job.sh
+++ Task1/Q3.2/DFW-ORD-DFW/mapreduce_job.sh
@@ -1,6 +1,6 @@
 /root/Hadoop/hadoop-2.7.3/bin/hadoop jar /root/Hadoop/hadoop-2.7.3/share/hadoop/tools/lib/hadoop-streaming-2.7.3.jar \
  -input /TransData/MyData.csv \
  -output /Q3.2/DFW-ORD-DFW \
- -file /home/ubuntu/Task1/Q7/DFW-ORD-DFW/mapper.py /home/ubuntu/Task1/Q7/DFW-ORD-DFW/reducer.py \
+ -file /home/ubuntu/Task1/Q3.2/DFW-ORD-DFW/mapper.py /home/ubuntu/Task1/Q3.2/DFW-ORD-DFW/reducer.py \
  -mapper "/usr/bin/python mapper.py" \
  -reducer "/usr/bin/python reducer.py"
