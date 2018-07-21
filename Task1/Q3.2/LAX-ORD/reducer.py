9458319dfc4dc2f8d2e829dbbe509026762a202a
Mostafa Hamdy Elgiar <mos.elgiar@gmail.com>
2018-07-21 09:28:34 +0200

    Update reducer.py

--- Task1/Q3.2/LAX-ORD/reducer.py
+++ Task1/Q3.2/LAX-ORD/reducer.py
@@ -9,9 +9,9 @@ for row in sys.stdin:
 	row = row.strip()
 	org_dest , dep_time , Arr_delay , flight_number = row.split(',')
 	if org_dest == 'LAX-ORD' and dep_time and int(dep_time) < 1200 :
-		output_dic_1[flight_number] = Arr_delay
+		output_dic_1[flight_number] = float(Arr_delay)
 	if org_dest == 'ORD-JFK' and dep_time and  int(dep_time) > 1200 :
-		output_dic_2[flight_number] = Arr_delay
+		output_dic_2[flight_number] = float(Arr_delay)
 
 output_dic_1 = sorted(output_dic_1.items() , key=itemgetter(1))
 output_dic_2 = sorted(output_dic_2.items() , key=itemgetter(1))
