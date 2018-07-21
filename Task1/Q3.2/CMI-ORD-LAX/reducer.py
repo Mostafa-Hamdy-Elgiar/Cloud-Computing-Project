a54484082ecb7f6679fdc6e7fc4ab8bde2488a77
Mostafa Hamdy Elgiar <mos.elgiar@gmail.com>
2018-07-21 09:23:17 +0200

    Update reducer.py

--- Task1/Q3.2/CMI-ORD-LAX/reducer.py
+++ Task1/Q3.2/CMI-ORD-LAX/reducer.py
@@ -10,12 +10,10 @@ for row in sys.stdin:
 	row = row.strip()
 	org_dest , dep_time , Arr_delay , flight_number = row.split(',')
 	if org_dest == 'CMI-ORD' and dep_time and int(dep_time) < 1200 :
-		output_dic_1[flight_number] = Arr_delay
+		output_dic_1[flight_number] = float(Arr_delay)
 	if org_dest == 'ORD-LAX' and dep_time and  int(dep_time) > 1200 :
-		output_dic_2[flight_number] = Arr_delay
+		output_dic_2[flight_number] = float(Arr_delay_
 
 output_dic_1 = sorted(output_dic_1.items() , key=itemgetter(1))
 output_dic_2 = sorted(output_dic_2.items() , key=itemgetter(1))
 print "CMI-ORD-LAX : " + output_dic_1[0][0] +','+output_dic_2[0][0]
-#print output_dic_1
-#print output_dic_2
